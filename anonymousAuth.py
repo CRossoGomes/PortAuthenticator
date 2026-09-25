722a1c11c5f0228ffe5cca7e4568996d71fcf270
Dragonite03 <dragonite03@Imigrante.home>
2026-09-25 11:46:03 -0300

    dividi em funções, até agr de scan, extração e display dos dados

--- anonymousAuth.py
+++ anonymousAuth.py
@@ -1,42 +1,59 @@
 import nmap
 import sys
 
-
 def scan_host(ip, portas="1-1024"):
     scanner = nmap.PortScanner()
-
+    
     print(f"[*] Iniciando varredura em {ip} (portas {portas})...")
-
+    
     try:
-        # -sV: detecção de versão dos serviços
-        # -sC: scripts padrão do Nmap (equivalente a --script=default)
+
         scanner.scan(ip, portas, arguments="-sV -sC")
+
     except nmap.PortScannerError as e:
+
         print(f"[!] Erro ao executar o Nmap: {e}")
         sys.exit(1)
 
+    return scanner
+
+def extrat_results(scanner):
+
+    results = []
     for host in scanner.all_hosts():
-        print(f"\nHost: {host} ({scanner[host].hostname()})")
-        print(f"Estado: {scanner[host].state()}")
-
         for proto in scanner[host].all_protocols():
-            print(f"\nProtocolo: {proto}")
             portas_abertas = scanner[host][proto].keys()
-
             for porta in sorted(portas_abertas):
+
                 info = scanner[host][proto][porta]
-                print(f"  Porta {porta}/{proto} - {info['state']}")
-                print(f"    Serviço: {info.get('name', 'desconhecido')}")
-                print(
-                    f"    Versão: {info.get('product', '')} {info.get('version', '')}"
-                )
+                
+                results.append({
+                    "host" : host   
+                    "proto" : proto
+                    "porta" : porta
+                    "estado" : info.get("state", "")
+                    "serviço" : info.get ("name" , "unknow")
+                    "produto" : info.get("product","")
+                    "versao" : info.get ("version" , "")
+                    "scripts" : info.get ("script" , {})
+                })
 
-                # resultados dos scripts (-sC)
-                if "script" in info:
-                    for script_nome, saida in info["script"].items():
-                        print(f"    [script:{script_nome}] {saida}")
+    return results
+
+def display_results(results):
+
+    for r in results: 
+
+        print(f" \nPorta {r ["porta"]}/{r["proto"]} - {r['state']}({r["host"]}) ")
+        print (f" {r["produto"]} {r["versao"]}")
+            for script_nome, saida in info['script'].items():
+                print(f"    [script:{script_nome}] {saida}")
 
 
+
+
+
+    
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Uso: python scanner.py <IP> [portas]")
@@ -45,3 +62,6 @@ if __name__ == "__main__":
     ip_alvo = sys.argv[1]
     portas = sys.argv[2] if len(sys.argv) > 2 else "1-1024"
     scan_host(ip_alvo, portas)
+
+
+    def ask_auth(porta):
